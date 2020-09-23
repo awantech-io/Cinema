8d6146da2255505895ba0e8199db69969403ae6b
awantech-io <hello.wanlab@gmail.com>
2020-09-23 10:05:01 +0800

    save data to local storage

--- script.js
+++ script.js
@@ -7,23 +7,38 @@ const movieSelect = document.getElementById('movie');
 // get selected movie ticket price in number format
 let ticketPrice = +movieSelect.value;
 
+// save selected movie index & price
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 // Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    
+
+    // set seat to array
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    //console.log(seatsIndex);
+
+    // save selected seat to localstorage
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     // get the total selected seat
     const selectedSeatsCount = selectedSeats.length;
     
     // put the seat count & total ticket price to display
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice; 
-
-    console.log(selectedSeatsCount);
 }
 
 // Movie select event
 movieSelect.addEventListener('change', e =>{
     ticketPrice = +e.target.value;
+    
+    // set selected movie index and price
+    setMovieData(e.target.selectedIndex, e.target.value);
+
     updateSelectedCount();
 });
 
