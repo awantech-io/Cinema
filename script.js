bfecb3b1f28e7ecaaf45f72c53765379c6266c74
awantech-io <hello.wanlab@gmail.com>
2020-09-23 10:50:29 +0800

    pull data from localstorage to UI

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+populateUi();
 
 // get selected movie ticket price in number format
 let ticketPrice = +movieSelect.value;
@@ -12,14 +14,15 @@ function setMovieData(movieIndex, moviePrice){
     localStorage.setItem('selectedMovieIndex', movieIndex);
     localStorage.setItem('selectedMoviePrice', moviePrice);
 }
+
 // Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-
+    
     // set seat to array
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
 
-    //console.log(seatsIndex);
+    console.log(seatsIndex);
 
     // save selected seat to localstorage
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
@@ -30,6 +33,28 @@ function updateSelectedCount() {
     // put the seat count & total ticket price to display
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice; 
+}
+
+// get dat afrom local storage and put in UI
+function populateUi(){
+
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    
+    // set selected seats
+    if (selectedSeats !== null && selectedSeats.length > 0 ) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    // set selected movie
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectedMovieIndex !== null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+    
 }
 
 // Movie select event
@@ -49,8 +74,10 @@ container.addEventListener('click', (e) => {
     !e.target.classList.contains('occupied')){
         // toggle the seat selected or unselected
         e.target.classList.toggle('selected');
-
+       
         updateSelectedCount();
     }
 });
 
+// Inital count and total set
+updateSelectedCount();
